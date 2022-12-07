5399422d60488305d43eb15703c4bbb12814cff9
flyisland <flyisland@gmail.com>
2022-12-07 20:47:57 +0800

    fix: change the center

--- config.mjs
+++ config.mjs
@@ -4,8 +4,8 @@ const config = {
 
   mapOptions: {
     //    center: { lat: -30.777444938024253, lng: 121.50542285547441 }, // Super Pit
-    center: { lat: -23.3598290, lng: 119.6702020 }, // Newman Mine Site
-    zoom: 15,
+    center: { lat: -23.3627438, lng: 119.6652746 }, // Newman Mine Site
+    zoom: 16,
     mapId: "DEMO_MAP_ID",
     mapTypeId: 'hybrid',
     fullscreenControl: false,
